33975a2f47e8ed5d9dffe6df6a92f99b4b3ff155
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2025-01-31 13:37:38 +0000

    removed hardcoded well done message from quiz page

--- src/Pages/QuizPage.js
+++ src/Pages/QuizPage.js
@@ -1,25 +1,24 @@
 import QuestionTypeOne from "../Components/QuestionTypeOne/QuestionTypeOne";
 
 export default function QuizPage({ name }) {
-	const quizData = [
-		{
-			questionType: 1,
-			questionColours: ["yellow", "blue"],
-			answerColor: "green",
-		},
-		{
-			questionType: 2,
-			questionColours: ["orange"],
-			answerColors: ["red", "yellow"],
-		},
-		// Add more questions as needed
-	];
+  const quizData = [
+    {
+      questionType: 1,
+      questionColours: ["yellow", "blue"],
+      answerColor: "green",
+    },
+    {
+      questionType: 2,
+      questionColours: ["orange"],
+      answerColors: ["red", "yellow"],
+    },
+    // Add more questions as needed
+  ];
 
-	return (
-		<>
-			{console.log(quizData[1])}
-			<QuestionTypeOne name = {name}/>
-			<h1>well done {name}</h1>
-		</>
-	);
+  return (
+    <>
+      {console.log(quizData[1])}
+      <QuestionTypeOne name={name} />
+    </>
+  );
 }
